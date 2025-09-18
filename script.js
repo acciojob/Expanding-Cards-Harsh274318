95daf48651a352365c2389afd53da445322b55ce
Harsh Vardhan Pal <hp77335@gmail.com>
2025-09-18 21:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+    let panels = document.querySelectorAll('.panel');
+
+for (let i = 0; i < panels.length; i++) {
+      panels[i].addEventListener('click', function() {
+        removeActiveClasses();
+        panels[i].classList.add('active');
+      });
+    }
+
+    function removeActiveClasses() {
+      for (let i = 0; i < panels.length; i++) {
+        panels[i].classList.remove('active');
+      }
+    }
